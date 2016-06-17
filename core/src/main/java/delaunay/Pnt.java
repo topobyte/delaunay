9654ced05bccf574bd55b314d48d6cd135f935d0
Sebastian Kürten <sebastian@topobyte.de>
2016-06-17 12:17:19 +0200

    Added additional copyright headers

--- core/src/main/java/delaunay/Pnt.java
+++ core/src/main/java/delaunay/Pnt.java
@@ -1,3 +1,23 @@
+// Original work Copyright 2005, 2007 by L. Paul Chew.
+// Modified work Copyright 2016 Sebastian Kuerten
+//
+// This file is part of delaunay.
+//
+// delaunay is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// delaunay is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with delaunay. If not, see <http://www.gnu.org/licenses/>.
+//
+// This is the header contained in the original file:
+
 /*
  * Copyright (c) 2005, 2007 by L. Paul Chew.
  *
