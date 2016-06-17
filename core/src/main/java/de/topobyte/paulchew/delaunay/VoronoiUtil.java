d239ef7c5612dcc1aec50abc92e23f6230452171
Sebastian Kürten <sebastian@topobyte.de>
2016-06-17 14:58:40 +0200

    Method for getting the Voronoi cells as geometries
    * Added VoronoiUtil class that shall contain various methods that are
      related to the Voronoi diagram.
    * Added jts and jts-utils as a dependency
    * Implemented a getVoronoiCells() method in the VoronoiUtil class

---
+++ core/src/main/java/de/topobyte/paulchew/delaunay/VoronoiUtil.java
@@ -0,0 +1,81 @@
+// Copyright 2016 Sebastian Kuerten
+//
+// This file is part of delaunay.
+//
+// delaunay is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// delaunay is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with delaunay. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.paulchew.delaunay;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Polygon;
+
+import de.topobyte.jts.utils.JtsHelper;
+import de.topobyte.jts.utils.PolygonHelper;
+
+/**
+ * Several methods that are provided for for speeding up the generation of a
+ * delaunay triangulation.
+ * 
+ * @author Sebastian Kuerten (sebastian@topobyte.de)
+ */
+public class VoronoiUtil
+{
+
+	/**
+	 * Create a mapping from site data objects to Voronoi cells.
+	 * 
+	 * @return the dual Voronoi cells.
+	 */
+	public static <T> Map<T, Geometry> getVoronoiCells(Triangulation<T> t)
+	{
+		Set<Pnt> done = new HashSet<>(t.getInitialTriangle());
+		GeometryFactory factory = new GeometryFactory();
+		Map<T, Geometry> map = new HashMap<>();
+		for (Triangle triangle : t) {
+			for (Pnt site : triangle) {
+				if (done.contains(site))
+					continue;
+				done.add(site);
+
+				T thing = t.getData().get(site);
+				List<Triangle> list = t.surroundingTriangles(site, triangle);
+				List<Double> xs = new ArrayList<>(list.size());
+				List<Double> ys = new ArrayList<>(list.size());
+				Pnt[] vertices = new Pnt[list.size()];
+				int i = 0;
+				for (Triangle tri : list) {
+					Pnt ccenter = tri.getCircumcenter();
+					vertices[i++] = ccenter;
+					xs.add(ccenter.coord(0));
+					ys.add(ccenter.coord(1));
+				}
+				LinearRing ring = JtsHelper.toLinearRing(xs, ys, false);
+				Polygon polygon = PolygonHelper.polygonFromLinearRing(ring,
+						factory);
+				map.put(thing, polygon);
+			}
+		}
+		return map;
+	}
+
+}
